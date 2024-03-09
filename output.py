4aa70a39e12fb77ad0ff6207ebf04fdf19369726
agentmistz <maxtenenbaum223@gmail.com>
2024-03-09 11:27:54 -0800

    Added overwrite error handling

--- output.py
+++ output.py
@@ -1,6 +1,6 @@
 import os
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 import pandas as pd
 import numpy as np
 
@@ -12,7 +12,11 @@ class Visualization:
         root = tk.Tk()
         root.withdraw
 
-        self.output_path = filedialog.asksaveasfilename(title = 'Select a save location')
+        self.output_path = filedialog.asksaveasfilename(
+            title = 'Select a save location',
+            defaultextension = '.csv',
+            filetypes=[("CSV files", "*.csv")]
+            )
 
         if not self.output_path:
             raise ValueError("No path selected.")
@@ -22,9 +26,31 @@ class Visualization:
     
 
     def output_as_csv(self, result_dataframe):
-        filename = f'{self.output_path}.csv'
-        result_dataframe.to_csv(filename, index=False)
-        print(f'Data written to {filename}')
+            while True:
+                root = tk.Tk()
+                root.withdraw()
+
+                filename = f'{self.output_path}'
+
+                # Check if the file exists
+                if os.path.exists(filename):
+                    overwrite = messagebox.askyesno("Overwrite File", f"{filename} already exists. Overwrite?")
+                    if not overwrite:
+                        root.destroy()
+                        self.open_save_dialog()  # Re-open save dialog to choose a new location
+                        continue  # Restart the loop with the new path
+                    else:
+                        break  # Exit the loop if the user agrees to overwrite
+                else:
+                    break  # Exit the loop if the file doesn't exist
+
+                root.destroy()
+
+            # Export the dataframe to CSV
+            result_dataframe.to_csv(filename, index=False)
+            #messagebox.showinfo("Info", f'Data written to {filename}')
+
+
 
 
         
